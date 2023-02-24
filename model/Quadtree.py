ed6f16b391cf47497fa4f23566a55edfd8038920
Emilien <langloisemilien@outlook.fr>
2023-02-24 10:06:22 +0100

    QuadTree.py

--- model/Quadtree.py
+++ model/Quadtree.py
@@ -21,55 +21,56 @@ class Node():
         self.y0 = y0
         self.w = width
         self.points = points
-        self.children = [] 
+        self.children = []
 
     #si c'est une feuille(carré) on a besoin des points qu'elle contient 
     def get_points(self):
         return self.points
 
-def division(node, m):
-    if(len(node.points)>m):
-        w2 = float(node.w /2)
-    
-        node_Point = PointsIn(node.x0, node.y0, w2, node.points)
-        n1 = Node(node.x0, node.y0, w2,node_Point)
-        division(n1,m)
+    def division(self, m):
+        if(len(self.points)>m):
+            w2 = float(self.w /2)
 
-        node_Point2 = PointsIn(node.x0, node.y0+w2, w2, node.points)
-        n2 = Node(node.x0, node.y0+w2, w2, node_Point2)
-        division(n2,m)
+            node_Point = self.PointsIn(self.x0, self.y0, w2)
+            n1 = Node(self.x0, self.y0, w2,node_Point)
+            n1.division(m)
 
-        node_Point3 = PointsIn(node.x0+w2, node.y0 ,w2, node.points)
-        n3 = Node(node.x0, node.y0, w2, node_Point3)
-        division(n3,m)
+            node_Point2 = self.PointsIn(self.x0, self.y0+w2, w2)
+            n2 = Node(self.x0, self.y0+w2, w2, node_Point2)
+            n2.division(m)
 
-        node_Point4 = PointsIn(node.x0+w2, node.y0+w2, w2, node.points)
-        n4 = Node(node.x0+w2, node.y0+w2, w2, node_Point4)
-        division(n4,m) 
+            node_Point3 = self.PointsIn(self.x0+w2, self.y0 ,w2)
+            n3 = Node(self.x0, self.y0, w2, node_Point3)
+            n3.division(m)
 
-        node.children = [n1,n2,n3,n4]
+            node_Point4 = self.PointsIn(self.x0+w2, self.y0+w2, w2)
+            n4 = Node(self.x0+w2, self.y0+w2, w2, node_Point4)
+            n4.division(m)
 
-def PointsIn(x,y,w,points):
-    pts = []
-    for p in points:
-        if ((x <= p.x) and (y <= p.y) and (x + w >= p.x) and (y + w >= p.y)):
-            pts.append(p.x)
-    return pts
+            self.children = [n1,n2,n3,n4]
+
+    def PointsIn(self,x,y,w):
+        pts = []
+        for p in self.points:
+            if ((x <= p.x) and (y <= p.y) and (x + w >= p.x) and (y + w >= p.y)):
+                pts.append(p)
+        return pts
 
 class QuadTree():
     def __init__(self, m):
         self.m = m
         self.points = [Point(random.randint(0,800),random.randint(0,800)) for i in range(20)]
         self.node0 = Node(0,0,800,self.points)
+        self.div()
 
     def addPoint(self,x,y):
         self.points.append(Point(x,y))
     
     def div(self):
-        division(self.node0,self.m)
+        Node.division(self.node0,self.m)
     
     def afficher(self):
-        #quelques testes 
+        #quelques tests
         print("nombre de points : ", len(self.points) == 20)
         print(self.points[0].y)
         # self.div()
