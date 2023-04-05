8cc4a4bec92b01b7ff3a78b4d0e91745c231ac49
Emilien <langloisemilien@outlook.fr>
2023-04-05 21:20:04 +0200

    delete views

--- Main.py
+++
@@ -1,7 +0,0 @@
-from view.ViewTestPygame import ViewTestPygame
-from view.testChart import TestChart
-import pygame, sys
-
-if __name__ == '__main__':
-    nCorps = ViewTestPygame()
-    nCorps.menu()
