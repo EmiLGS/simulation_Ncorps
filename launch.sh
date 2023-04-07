2c3ef7502b375560918d0dba821ab7201042d448
Emilien Langlois <22002270@V304V-FCP032.campus.unicaen.fr>
2023-04-07 14:15:52 +0200

    Fix script

--- launch.sh
+++ launch.sh
@@ -7,20 +7,36 @@ is_pip_installed()
     pip list 2> /dev/null | grep -qv "$1"
 }
 
-if ! is_installed python3;
-    then
-        sudo apt-get install python3.10;
+if is_installed python3;
+
+then
+
+sudo apt-get install python3.10;
+
 fi
-if ! is_installed pip;
-    then
-        python3 get-pip.py;
+
+if is_installed pip;
+
+then
+
+python3 get-pip.py;
+
 fi
-if ! is_pip_installed pygame;
-    then
-        python3 -m pip install -U pygame --user;
+
+if is_pip_installed pygame;
+
+then
+
+python3 -m pip install -U pygame --user;
+
 fi
-if ! is_pip_installed matplotlib;
-    then
-        pip install matplotlib
+
+if is_pip_installed matplotlib;
+
+then
+
+pip install matplotlib
+
 fi
+
 python3 Main.py
